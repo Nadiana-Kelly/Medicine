624673f60a95e9c5b4d74af46e7ae094cc12d841
Nadiana-Kelly <nadianakelly1@gmail.com>
2023-07-02 12:02:25 -0300

    banco de dados atualizado

--- bd.sql
+++ bd.sql
@@ -3,14 +3,14 @@
 -- DROP DATABASE IF EXISTS medicine;
 
 -- CREATE DATABASE medicine
---     WITH
---     OWNER = postgres
---     ENCODING = 'UTF8'
---     LC_COLLATE = 'Portuguese_Brazil.1252'
---     LC_CTYPE = 'Portuguese_Brazil.1252'
---     TABLESPACE = pg_default
---     CONNECTION LIMIT = -1
---     IS_TEMPLATE = False;
+--    WITH
+--    OWNER = postgres
+--    ENCODING = 'UTF8'
+--    LC_COLLATE = 'Portuguese_Brazil.1252'
+--    LC_CTYPE = 'Portuguese_Brazil.1252'
+--    TABLESPACE = pg_default
+--    CONNECTION LIMIT = -1
+--    IS_TEMPLATE = False;
 
 -- CREATE TABLE usuarios_registrados (
 -- 	  ID SERIAL PRIMARY KEY,
@@ -21,35 +21,37 @@
 -- 	  endereco_completo VARCHAR(200),
 -- 	  telefone VARCHAR(20),
 -- 	  email VARCHAR(100),
--- 	  username VARCHAR(50),
+-- 	  username VARCHAR(50) UNIQUE,
 -- 	  senha VARCHAR(50)
 -- );
 
--- SELECT * FROM usuarios_registrados
-
 -- CREATE TABLE agendamento (
---   ID SERIAL PRIMARY KEY,
---   nome_medico VARCHAR(100),
---   data_consulta DATE,
---   horario TIME,
---   convenio_medico VARCHAR(100),
---   motivo_consulta VARCHAR(300)
+--    ID SERIAL PRIMARY KEY,
+--    nome_medico VARCHAR(100),
+--    data_consulta DATE,
+--    horario TIME,
+--    convenio_medico VARCHAR(100),
+--    motivo_consulta VARCHAR(300),
+--    id_paciente INTEGER,
+--    id_medico INTEGER,
+--    diasemana VARCHAR(20)
 -- );
-
--- SELECT * FROM agendamento
 
 -- CREATE TABLE medicos (
---   ID SERIAL PRIMARY KEY,
---   nome VARCHAR(100),
---   area_medica VARCHAR(100),
---   descricao TEXT,
---   username VARCHAR(50),
---   senha VARCHAR(50)
+--    ID SERIAL PRIMARY KEY,
+-- 	  nome VARCHAR(100),
+-- 	  area_medica VARCHAR(100),
+-- 	  descricao TEXT,
+-- 	  username VARCHAR(50) UNIQUE,
+-- 	  senha VARCHAR(50)
 -- );
 
--- ALTER TABLE agendamento ADD id_paciente INTEGER;
-
--- SELECT * FROM agendamento
+-- CREATE TABLE horarios(
+-- 	  ID SERIAL PRIMARY KEY,
+-- 	  id_medico INTEGER,
+-- 	  disemana VARCHAR(20),
+-- 	  horario TIME
+-- );
 
 
 
