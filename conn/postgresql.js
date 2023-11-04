83a6232c8478ce492b85e80c628c2411e26ef8d9
Nadiana Kelly <nadianakelly1@gmail.com>
2023-11-04 18:17:32 -0300

    REFATORAÇÃO do header e footer

--- conn/postgresql.js
+++ conn/postgresql.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',
-  host: 'medicine_database',
+  host: 'localhost',
   database: 'medicine',
   password: '123',
   port: 5433 // porta padrão do PostgreSQL
